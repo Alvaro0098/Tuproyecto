7a104aad488371f32adfb0f973dcebcbe2d91ddc
Alfonso Garay <alfonsogaray03@gmail.com>
2022-09-27 19:11:21 -0300

    27/09

--- src/app/Interfaces/Contacts.ts
+++ src/app/Interfaces/Contacts.ts
@@ -6,3 +6,10 @@ export interface Contact {
     PhoneNumber: string,
 }
 
+
+// Cree una interfas que se usa para generar el tipo de dato del Contact
+// seria como instanciar una clase pero donde creo la clase tipo Contact
+// 'export' sirve para poder importar la interfas en otro archivo.
+
+//Por lo general las interfase se usa para el manejo de datos, que es mucho mas simple
+// Las clases quedan reservadas para generar modulos.
